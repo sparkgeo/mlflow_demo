723ab2b2ff109cef64c2d7d83d0912dc92a70234
Greg Nieuwenhuis <26285069+gnieuwenhuis@users.noreply.github.com>
2024-02-06 09:51:23 -0700

    app name update

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Simple MLFlow Server in AWS',
+    name='mlflow_simple_aws',
     version='0.0.1',
     author='Greg Nieuwenhuis',
     author_email='gnieuwenhuis@sparkgeo.com',
-    description='A short description of your project',
+    description='Simple MLFlow Server in AWS',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
